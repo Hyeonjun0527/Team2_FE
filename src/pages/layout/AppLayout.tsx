68a18fb720b5dc997a548f443d8f4c6e90c530f2
anseonghyeon <anseonghyeon@gmail.com>
2025-10-09 20:46:52 +0900

    style: prettier 돌림

--- src/pages/layout/AppLayout.tsx
+++ src/pages/layout/AppLayout.tsx
@@ -91,7 +91,7 @@ function AppLayout() {
 
     // 컴포넌트 언마운트 시 SSE 연결 정리
     return () => {
-      console.log("컼포넌트 언마운트 되엇음");
+      console.log('컼포넌트 언마운트 되엇음');
       console.log('[SSE] 연결 종료 (cleanup)');
       es.close();
     };
